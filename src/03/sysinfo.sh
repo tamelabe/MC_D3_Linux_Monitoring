ebdfe485b9dfbc4a325647353355181285474833
Tamela Bears <tamelabe@student.21-school.ru>
2022-10-24 10:51:02 +0300

    Transfer to an PC

--- src/03/sysinfo.sh
+++ src/03/sysinfo.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+HOSTNAME=$(hostname)
+TIMEZONE="$(cat /etc/timezone) UTC $(date +"%:z")"
+USER=$(whoami)
+OS=$(grep 'PRETTY_NAME=' /etc/os-release) | awk -F"\"" '{print ($2)}'
+DATE=$(date) | awk '{print ($2,$3,$4, $5)}'
+UPTIME=$(uptime --pretty)
+UPTIME_SEC=$(cat /proc/uptime | awk '{print $1}')
+IP=$(ifconfig enp0s3) | grep "inet " | awk '{print ($2)}'
+MASK=$(ifconfig enp0s3) | grep "inet " | awk '{print ($4)}'
+GATEWAY=$(ip route) | grep "default" | awk '{print ($3)}'
+RAM_TOTAL=$(free -m) | grep Mem: | awk '{printf ("%.3f GB\n", $2/1024)}'
+RAM_USED=$(free -m) | grep Mem: | awk '{printf ("%.3f GB\n", $3/1024)}'
+RAM_FREE=
+SPACE_ROOT=
+SPACE_ROOT_USED=
+SPACE_ROOT_FREE=
+
 function info_echo {
     echo "HOSTNAME = $(hostname)"
     echo "TIMEZONE = $(cat /etc/timezone) UTC $(date +"%:z")"
@@ -17,13 +34,4 @@ function info_echo {
     echo "$(df /root/)" | grep "\/$" | awk '{printf ("SPACE_ROOT = %.2f Mb\n", $2/1024)}'
     echo "$(df /root/)" | grep "\/$" | awk '{printf ("SPACE_ROOT_USED = %.2f Mb\n", $3/1024)}'
     echo "$(df /root/)" | grep "\/$" | awk '{printf ("SPACE_ROOT_FREE = %.2f Mb\n", $4/1024)}'
-}
-
-function question {
-    read -p "Do you want to write this information in file (Y/N)?" ans
-    if [ "$ans" = "y" ] || [ "$ans" = "Y" ]; then
-        current_date=$(date "+%d_%m_%y_%H_%M_%S")
-        filename="$current_date.status"
-        info_echo >> $filename
-    fi
 }
