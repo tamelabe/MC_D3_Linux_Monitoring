0c13a0d9dee86096e7369e68c6490417fd9b838c
Tamela Bears <tamelabe@student.21-school.ru>
2022-10-26 20:29:07 +0300

    05 partly

--- src/05/script.sh
+++ src/05/script.sh
@@ -25,10 +25,16 @@ function diff_files_count {
 
 function top10_files {
     echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
+    find $1 -type f -exec du -h {} + | sort -hr | head | awk '{printf "%d - %s, %s", NR, $2, $1; if (match($2, /[ -.0-~]\.[ -.0-~]+/)) printf ", %s\n", substr($2,RSTART+2,RLENGTH); else printf "\n"}'
 }
 
 function top10_exec_files {
     echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file):"
+    find $1 -type f -executable -exec du -h {} + | sort -hr | head > info.tmp
+    awk '{system("md5sum " $2)}' info.tmp | awk '{print $1}' > hashes.tmp
+    awk '{printf "%d - %s, %s, \n", NR, $2, $1}' info.tmp > info_style.tmp
+    awk 'FNR==NR {a[NR]=$1; next} {$5=a[FNR]}1' hashes.tmp info_style.tmp
+    rm info.tmp info_style.tmp hashes.tmp
 }
 
 function run_time {
